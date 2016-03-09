d6ebfe40ee2bbde611652f849218937b5d8a23d7
haydenbbickerton <haydenbbickerton@gmail.com>
2016-03-09 15:23:35 -0600

    Use standardjs.com style.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Chart from './components/chart'
 
 function install (Vue, options = {}) {
-    Vue.component('vue-chart', Chart)
+  Vue.component('vue-chart', Chart)
 }
 
 export default install
--- src/utils/googleChartsLoader.js
+++ src/utils/googleChartsLoader.js
@@ -6,51 +6,51 @@ let is_loaded = false
     promise function itself.
  */
 let makeDeferred = function () {
-    let resolvePromise = null
-    let rejectPromise = null
+  let resolvePromise = null
+  let rejectPromise = null
 
-    let promise = new Promise(function (resolve, reject) {
-        resolvePromise = resolve
-        rejectPromise = reject
-    })
+  let promise = new Promise(function (resolve, reject) {
+    resolvePromise = resolve
+    rejectPromise = reject
+  })
 
-    return {
-        promise: promise,
-        resolve: resolvePromise,
-        reject: rejectPromise
-    }
+  return {
+    promise: promise,
+    resolve: resolvePromise,
+    reject: rejectPromise
+  }
 }
 
 // Our main promise
 let google_promise = makeDeferred()
 
 export default (packages, version) => {
-
-    // Google only lets you load it once, so we'll only run once.
-    if (is_loading || is_loaded) {
-        return google_promise.promise
-    }
-
-    is_loading = true
-
-    let script = document.createElement('script')
-    script.setAttribute('src', 'https://www.gstatic.com/charts/loader.js')
-
-    script.onreadystatechange = script.onload = function () {
-
-        // After the 'loader.js' is loaded, load our version and packages
-        google.charts.load(version, { packages: packages })
-
-        // After we've loaded Google Charts, resolve our promise
-        google.charts.setOnLoadCallback(function () {
-            is_loading = false
-            is_loaded = true
-            google_promise.resolve()
-        })
-    }
-
-    // Insert our script into the DOM
-    document.getElementsByTagName('head')[0].appendChild(script)
-
+  // Google only lets you load it once, so we'll only run once.
+  if (is_loading || is_loaded) {
     return google_promise.promise
+  }
+
+  is_loading = true
+
+  let script = document.createElement('script')
+  script.setAttribute('src', 'https://www.gstatic.com/charts/loader.js')
+
+  script.onreadystatechange = script.onload = function () {
+    // After the 'loader.js' is loaded, load our version and packages
+    google.charts.load(version, {
+      packages: packages
+    })
+
+    // After we've loaded Google Charts, resolve our promise
+    google.charts.setOnLoadCallback(function () {
+      is_loading = false
+      is_loaded = true
+      google_promise.resolve()
+    })
+  }
+
+  // Insert our script into the DOM
+  document.getElementsByTagName('head')[0].appendChild(script)
+
+  return google_promise.promise
 }
